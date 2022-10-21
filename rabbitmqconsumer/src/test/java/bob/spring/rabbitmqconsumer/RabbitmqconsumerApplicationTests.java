3cff334c33a45b14fb2265d2cfca4261383e13f2
MrDujing <zhangbaochn@outlook.com>
2022-10-21 14:09:26 +0800

    init rabbitmqproducer and rabbitmqconsumer

---
+++ rabbitmqconsumer/src/test/java/bob/spring/rabbitmqconsumer/RabbitmqconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package bob.spring.rabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ rabbitmqproducer/src/test/java/bob/spring/rabbitmqproducer/RabbitmqproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package bob.spring.rabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqproducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

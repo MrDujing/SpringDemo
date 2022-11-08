2a875d83b03be9cbd2ef9a12180fa19a50657d38
MrDujing <zhangbaochn@outlook.com>
2022-11-08 10:22:47 +0800

    config server

---
+++ springconfigclient/src/test/java/bob/spring/springconfigclient/SpringconfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package bob.spring.springconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringconfigclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
